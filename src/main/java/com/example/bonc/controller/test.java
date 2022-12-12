b38b06a906df8881f69c35a410292e8cf0faa383
tanhao <woaiwojia1822797>
2022-12-12 10:07:45 +0800

    测试

---
+++ src/main/java/com/example/bonc/controller/test.java
@@ -0,0 +1,11 @@
+package com.example.bonc.controller;
+
+/**
+ * @Author TanHao
+ * @Date 2022/12/12 0012 10:06
+ */
+public class test {
+    private String weekFilename;
+    private String attendanceSheetFilename;
+    private String path;
+}
